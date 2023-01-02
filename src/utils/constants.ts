db69950e567eed9b9761fe6ce0353a75eb6f2d54
JustAPie <justapie06@gmail.com>
2023-01-02 10:26:07 +0700

    add more intents

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,9 @@ import { GatewayIntentBits, IntentsBitField } from "discord.js";
 
 export const GATEWAY_INTENTS: GatewayIntentBits[] = [
     IntentsBitField.Flags.Guilds,
-    IntentsBitField.Flags.GuildMembers
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildPresences,
+    IntentsBitField.Flags.MessageContent
 ];
 
 export const ENV: string = process.env.NODE_ENV;
