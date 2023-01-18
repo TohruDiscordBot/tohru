15e2dca6394aa33cacbc6d426f939bae6a40d274
JustAPie <justapie06@gmail.com>
2023-01-18 20:30:34 +0700

    remove permission

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -1,7 +1,6 @@
 import { ChatInputApplicationCommandData, CommandInteraction, PermissionFlags } from "discord.js";
 
 export interface Command extends ChatInputApplicationCommandData {
-    userPermissions?: PermissionFlags[],
     preconditions?: string[],
     run(interaction: CommandInteraction): Promise<void>
 };
