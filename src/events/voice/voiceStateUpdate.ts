91ee73e6f3fa66e3a85e61d1e4c1528e49717e58
JustAPie <justapie06@gmail.com>
2023-01-24 18:03:49 +0700

    end stage immediately after empty voice

--- src/events/voice/voiceStateUpdate.ts
+++ src/events/voice/voiceStateUpdate.ts
@@ -10,14 +10,19 @@ client.on("voiceStateUpdate", async (oldState: VoiceState, newState: VoiceState)
         if (musicCfg[247]) return;
 
         if (oldState.channel.members.has(client.user.id)) {
-
             let members: number = oldState.channel.members.filter(
                 (val: GuildMember) => !val.user.bot
             ).size;
 
             if (members === 0 || oldState.member === await oldState.guild.members.fetchMe()) {
-                await oldState.disconnect();
+                await newState.disconnect();
                 await cluster.destroyPlayer(oldState.guild.id);
+
+                if (oldState.channel instanceof StageChannel) {
+                    const stage: StageChannel = oldState.channel;
+
+                    await stage.stageInstance.delete();
+                }
             }
         }
     }
