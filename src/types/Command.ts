860d3bad48cbdd8eb9c1531742c6dbddc3f47ae1
JustAPie <justapie06@gmail.com>
2023-01-24 10:59:37 +0700

    allow ephemeral command

--- src/types/Command.ts
+++ src/types/Command.ts
@@ -2,5 +2,6 @@ import { ChatInputApplicationCommandData, CommandInteraction } from "discord.js"
 
 export interface Command extends ChatInputApplicationCommandData {
     preconditions?: string[],
+    ephemeral?: boolean,
     run(interaction: CommandInteraction): Promise<void>
 };
