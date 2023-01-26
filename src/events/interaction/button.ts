f6ea0ee51d18838f7fc354cd011e6898004107e1
JustAPie <justapie06@gmail.com>
2023-01-26 11:26:03 +0700

    change button system

--- src/events/interaction/button.ts
+++ src/events/interaction/button.ts
@@ -1,6 +1,5 @@
 import { ButtonInteraction, Interaction } from "discord.js";
 import { client } from "../../client.js";
-import { Button } from "../../types/Button.js";
 import { logger } from "../../utils/logger.js";
 
 client.on("interactionCreate", async (interaction: Interaction) => {
@@ -8,7 +7,7 @@ client.on("interactionCreate", async (interaction: Interaction) => {
         await interaction.deferUpdate();
         logger.info(`[INTERACTION] [SHARD #${interaction.guild.shard.id}] User ${interaction.user.id} at guild ${interaction.guildId} requested button ${interaction.customId}.`);
 
-        const button: Button = client.buttons.get(interaction.customId);
+        const button = client.buttons.get(interaction.customId);
 
         if (!button) {
             logger.warn(`[INTERACTION] [SHARD #${interaction.guild.shard.id}] Button ${interaction.customId} not found. Cancelling...`);
@@ -16,7 +15,7 @@ client.on("interactionCreate", async (interaction: Interaction) => {
         }
 
         try {
-            await button.run(interaction);
+            await button(interaction);
             logger.info(`[INTERACTION] [SHARD #${interaction.guild.shard.id}] Button ${interaction.customId} executed.`);
             logger.debug(`[INTERACTION] [SHARD #${interaction.guild.shard.id}] Interaction ID: ${interaction.id}.`);
             logger.debug(`[INTERACTION] [SHARD #${interaction.guild.shard.id}] Interaction token: ${interaction.token}.`);
--- src/types/Button.ts
+++
@@ -1,5 +0,0 @@
-import { ButtonComponent, ButtonInteraction } from "discord.js";
-
-export interface Button extends ButtonComponent {
-    run(interaction: ButtonInteraction): Promise<void>
-}
