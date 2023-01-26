653557dfbe8ffc503f93d90257ba802cc951ff65
JustAPie <justapie06@gmail.com>
2023-01-26 11:26:18 +0700

    format code

--- src/events/voice/voiceStateUpdate.ts
+++ src/events/voice/voiceStateUpdate.ts
@@ -9,12 +9,12 @@ client.on("voiceStateUpdate", async (oldState: VoiceState, newState: VoiceState)
     if (oldState.channelId && !newState.channelId) {
         if (musicCfg[247]) return;
 
-        if (oldState.channel.members.has(client.user.id)) {
-            let members: number = oldState.channel.members.filter(
+        if (oldState.channel.members.get(oldState.client.user.id)) {
+            const members: number = oldState.channel.members.filter(
                 (val: GuildMember) => !val.user.bot
             ).size;
 
-            if (members === 0 || oldState.member === await oldState.guild.members.fetchMe()) {
+            if (members === 0 || oldState.member === (await oldState.guild.members.fetchMe())) {
                 await newState.disconnect();
                 await cluster.destroyPlayer(oldState.guild.id);
 
