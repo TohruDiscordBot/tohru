ad8e2cd57af9cab9f7d41aa5cd92f3ae195cf3a6
JustAPie <justapie06@gmail.com>
2023-02-05 12:00:36 +0700

    repeat code :V

---
+++ src/commands/music/filters.ts
@@ -0,0 +1,104 @@
+import { ActionRowBuilder, ButtonBuilder, ButtonInteraction, ButtonStyle, CommandInteraction, EmbedBuilder } from "discord.js";
+import { Player } from "lavaclient";
+import { cluster } from "../../modules/Music.js";
+import { clearFilters, toggleDaycore, toggleKaraoke, toggleNightcore, toggleVaporWave } from "../../utils/filterHelpers.js";
+import { registerButton, registerCommand } from "../index.js";
+
+const buttons: ButtonBuilder[] = [
+    new ButtonBuilder()
+        .setCustomId("f-nightcore")
+        .setLabel("🌃")
+        .setStyle(ButtonStyle.Primary),
+    new ButtonBuilder()
+        .setCustomId("f-daycore")
+        .setLabel("☀")
+        .setStyle(ButtonStyle.Primary),
+    new ButtonBuilder()
+        .setCustomId("f-karaoke")
+        .setLabel("🎤")
+        .setStyle(ButtonStyle.Primary),
+    new ButtonBuilder()
+        .setCustomId("f-vaporwave")
+        .setLabel("🌊")
+        .setStyle(ButtonStyle.Primary),
+    new ButtonBuilder()
+        .setCustomId("f-clear")
+        .setLabel("❎")
+        .setStyle(ButtonStyle.Primary)
+];
+
+registerCommand({
+    name: "filters",
+    description: "Spice up your music with filters.",
+    preconditions: ["activePlayer"],
+    async run(interaction: CommandInteraction): Promise<void> {
+        await render(interaction);
+    }
+});
+
+registerButton("f-nightcore",
+    async (interaction: ButtonInteraction) => {
+        await toggleNightcore(cluster.getPlayer(interaction.guildId));
+        await render(interaction);
+    }
+);
+
+registerButton("f-daycore",
+    async (interaction: ButtonInteraction) => {
+        await toggleDaycore(cluster.getPlayer(interaction.guildId));
+        await render(interaction);
+    }
+);
+
+registerButton("f-karaoke",
+    async (interaction: ButtonInteraction) => {
+        await toggleKaraoke(cluster.getPlayer(interaction.guildId));
+        await render(interaction);
+    }
+);
+
+registerButton("f-vaporwave",
+    async (interaction: ButtonInteraction) => {
+        await toggleVaporWave(cluster.getPlayer(interaction.guildId));
+        await render(interaction);
+    }
+);
+
+registerButton("f-clear",
+    async (interaction: ButtonInteraction) => {
+        await clearFilters(cluster.getPlayer(interaction.guildId));
+        await render(interaction);
+    }
+);
+
+async function render(interaction: CommandInteraction | ButtonInteraction): Promise<void> {
+    const player: Player = cluster.getPlayer(interaction.guildId);
+
+    const { filterStatus } = player;
+
+    const embed: EmbedBuilder = new EmbedBuilder()
+        .setAuthor({
+            name: interaction.guild.name,
+            iconURL: interaction.guild.iconURL({ extension: "png", forceStatic: false })
+        })
+        .setTitle("Filter controller");
+
+    for (const [key, val] of Object.entries(filterStatus)) {
+        embed.addFields({
+            name: key,
+            value: val ? "Enabled" : "Disabled",
+            inline: true
+        });
+    }
+
+    const row: ActionRowBuilder = new ActionRowBuilder()
+        .addComponents(buttons);
+
+    await interaction.editReply({
+        embeds: [embed],
+        components: [
+            // @ts-ignore
+            row
+        ]
+    });
+}
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -23,13 +23,6 @@ export const MODULE_OPTION: ApplicationCommandOption = {
     ]
 }
 
-export const DEFAULT_FILTER_STATUS = {
-    nightcore: false,
-    daycore: false,
-    karaoke: false,
-    vaporwave: false
-};
-
 export const YOUTUBE_URL_REGEX: RegExp = /^(?:(?:https|http):\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be).*(?<=\/|v\/|u\/|embed\/|shorts\/|watch\?v=)(?<!\/user\/)(?<id>[\w\-]{11})(?=\?|&|$)/;
 
 export const ENV: string = process.env.NODE_ENV.toLowerCase();
