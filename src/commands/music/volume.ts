327e8e17e962e265fdef890adc6ba9b130dc7613
JustAPie <justapie06@gmail.com>
2023-02-05 17:29:54 +0700

    add command to control volume

---
+++ src/commands/music/volume.ts
@@ -0,0 +1,36 @@
+import { ApplicationCommandOptionType, Colors, CommandInteraction } from "discord.js";
+import { Player } from "lavaclient";
+import { cluster } from "../../modules/Music.js";
+import { registerCommand } from "../index.js";
+
+registerCommand({
+    name: "volume",
+    description: "🔊 Controls the volume of Tohru.",
+    preconditions: ["activePlayer"],
+    options: [
+        {
+            name: "level",
+            description: "🔢 Volume level.",
+            type: ApplicationCommandOptionType.Number,
+            minValue: 0,
+            max_value: 200,
+            required: false
+        }
+    ],
+    async run(interaction: CommandInteraction): Promise<void> {
+        const player: Player = cluster.getPlayer(interaction.guildId);
+
+        const level: number = interaction.options.get("volume") ? 100 : interaction.options.get("volume").value as number;
+
+        await player.setVolume(level);
+
+        await interaction.editReply({
+            embeds: [
+                {
+                    color: Colors.Blue,
+                    description: `✅ Successfully set the volume to ${level}%`
+                }
+            ]
+        });
+    }
+});
