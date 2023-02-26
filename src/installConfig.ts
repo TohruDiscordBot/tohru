62489c08ab6f3b222642f67061d6760ed9f9fe81
JustAPie <justapie06@gmail.com>
2023-02-26 22:09:39 +0700

    reset connstring -> use envvar

--- src/installConfig.ts
+++ src/installConfig.ts
@@ -1,10 +1,8 @@
 import { DotenvAzure } from "dotenv-azure";
 import { mkdirSync, existsSync, writeFileSync } from "fs";
 
-const CONN_STR: string = "Endpoint=https://tohru.azconfig.io;Id=78t8-l0-s0:gwHLW5Lj3dD4g+C6b5iT;Secret=QMJQJVYYAmfctY1Hwe9fk0cVaO0d2MULKjcojogE9DE=";
-
 const azr: DotenvAzure = new DotenvAzure({
-    connectionString: CONN_STR
+    connectionString: process.env.AZURE_APP_CONFIGURATION_CONNECTION_STRING
 });
 
 const { parsed } = await azr.config();
